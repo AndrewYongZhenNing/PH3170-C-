6c644c907420c192b24f53c1e9460dea7fd55825
AndrewYongZhenNing <ayzn95@gmail.com>
2016-08-19 17:13:43 +0100

    last save before using arrays

---
+++ ProblemSheet2.cpp
@@ -0,0 +1,196 @@
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+
+int main(){
+  // QUESTION 3
+  // QUESTION 4a
+  // int x = 1;
+  // int for_sum = 0;
+  // int while_sum = 0;
+  //
+  // for(int x = 1; x <101; x++){
+  //   for_sum += x;
+  // }
+  // // repeat:
+  //
+  //
+  // while(x <101){
+  //   while_sum += x;
+  //   x += 1;
+  // }
+  // std::cout << "The sum from 1 to 100 is: " << while_sum << std::endl;
+  //
+  // // QUESTION 4b - generalising for-while loops
+  // int n1 = 0;
+  // int n2 = 0;
+  // int loop_selection = 0;
+  //
+  // std::cout << "Enter value for n1: ";
+  // std::cin >> n1;
+  // std::cout << "\nEnter value for n2: ";
+  // std::cin>> n2;                          // if n1 > n2, sum goes 0
+  //
+  // if(n1>n2){
+  //   std::cout << "n1 is greater than n2, please select the values such that n1 is less than n2" << std::endl;
+  //   // break;
+  // }
+  //
+  // std::cout << "For for loops, enter 1, for while loops, enter 2: " << std::endl;
+  // std::cin >> loop_selection;
+  //
+  //
+  // int gen_for_sum = 0;
+  // int gen_while_sum = 0;
+  //
+  // if(loop_selection == 1){
+  //   for(n1; n1<n2; n1+= 1){
+  //     gen_for_sum += n1;
+  //   }
+  //   std::cout << " SUM = " << gen_for_sum << std::endl;
+  // }
+  //
+  // else if(loop_selection == 2){
+  //   while(n1<n2){
+  //
+  //     gen_while_sum += n1;
+  //     n1 +=1;
+  //   }
+  //   std::cout << " SUM = " << gen_while_sum << std::endl;
+  // }
+  //
+  // else{
+  //   std::cout << "Invalid selection number, please try again \n";
+  // }
+
+
+  // QUESTION 4d
+
+  float total = 0;
+  int N = 0;
+  float mean = 0;
+  int negative_values = 0;
+  float maggiore = 0; //largest
+  float minore = 0; // smallest
+  float std_dev = 0;
+  float var_numerator = 0;
+
+
+  std::string contents;
+  std::ifstream myfile("simpledata.txt");
+
+  if (myfile.is_open()){
+
+
+    float number;
+
+    while (std::getline(myfile,contents)){
+
+      std::stringstream convert(contents);
+      convert >> number;
+
+      if (number < 0){ // looks for negative numbers
+        negative_values += 1;
+      }
+
+      if (number > maggiore){ // looks for largest number
+        maggiore = number;
+      }
+
+      if (number < minore){
+        minore = number;
+      }
+      // float number = atoi(contents.c_str())
+
+      total += number;
+      N += 1;
+
+      mean = total/N;
+
+      var_numerator += pow(mean-number,2);
+      std::cout << mean << std::endl;
+      std_dev = sqrt(var_numerator/N);
+
+      // std::cout << "std" << std_dev << std::endl; // do not un-coment this for now
+    }
+
+    // float number;
+
+    // if (total == 0){
+    //
+    //   float number;
+    //
+    //   while (std::getline(myfile,contents)){
+    //
+    //     std::stringstream convert(contents);
+    //     convert >> number;
+    //     // std::cout << contents << std::endl;
+    //     if (number < 0){
+    //       negative_values += 1;
+    //     }
+    //     // float number = atoi(contents.c_str())
+    //
+    //     total += number;
+    //     N += 1;
+    //   }
+    //
+    //   mean = total/N;
+    // }
+    //
+    //
+    // else if(total > 0){
+    //
+    //   float number;
+    //
+    //   while (std::getline(myfile,contents)){
+    //
+    //     std::cout << contents << std::endl;
+    //     std::stringstream convert(contents);
+    //     convert >> number;
+    //
+    //     var_numerator += pow(number - mean,2);
+    //
+    //   }
+    //
+    // }
+    //
+    // std_dev = sqrt(var_numerator/N);
+
+    myfile.close();
+
+
+    }
+
+  // else{
+  //
+  //   while (std::getline(myfile,contents)){
+  //
+  //     std::cout << contents << std::endl;
+  //     std::stringstream convert(contents);
+  //     convert >> number;
+  //
+  //     var_numerator += pow(number - mean,2);
+  //
+  //   }
+  //   myfile.close();
+  // }
+
+
+  std::cout << "variance " << var_numerator/N << std::endl;
+
+  std::cout << "Sum = " << total << std::endl;
+
+  std::cout << "\nMean =  " << mean << std::endl;
+
+  std::cout << "\nStandard deviation is = " << std_dev << std::endl;
+
+  std::cout << "\nNumber of negative values are = " << negative_values << std::endl;
+
+  std::cout << "\nLargest value = " << maggiore << "\n \nSmallest value = " << minore << std::endl;
+
+
+}
