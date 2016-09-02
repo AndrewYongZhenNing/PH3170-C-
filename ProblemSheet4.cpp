967a48963185d49b435e0f6bbbee3d3e85836b7f
AndrewYongZhenNing <ayzn95@gmail.com>
2016-09-02 20:58:26 +0100

    update: matrix & bubble sort
    matrix representation is correcto.
    can print sequence in horizontal using flush

--- ProblemSheet4.cpp
+++ ProblemSheet4.cpp
@@ -6,12 +6,28 @@ int main(){
 
   // QUESTION 3
 
+  std::cout << "Question 3: Inverse of a Matrix\n" << std::endl;
+
   int M[3][3] = {{5,7,3},{1,8,4},{2,7,6}};
+
+  std::cout<< "Consider matrix M: " << std::endl;
+
+  for (int i = 0; i < 3; i++){
+
+    std::cout << " " << std::endl;
+
+    for (int j = 0; j < 3; j++){
+
+      std::cout << M[i][j] << " " << std::flush;
+
+    }
+
+  }
 
   int determinant = M[0][0]*(M[1][1]*M[2][2] - M[1][2]*M[2][1]) - M[0][1]*(M[1][0]*M[2][2] - M[1][2]*M[2][0]) + M[0][2]*(M[1][0]*M[2][1] - M[1][1]*M[2][0]);
 
-  // int c11 = (M[1][1]*M[2][2] - M[1][2]*M[2][1]);
-  // std::cout << "cofactor11 " << c11 << std::endl;
+  std::cout << "\n \nDeterminant of M, \ndet M = " << determinant << std::endl;
+
   int cofactor[3][3] = {{(M[1][1]*M[2][2]-M[1][2]*M[2][1]),-(M[1][0]*M[2][2]-M[1][2]*M[2][0]),(M[1][0]*M[2][1] - M[1][1]*M[2][0])},{-(M[0][1]*M[2][2] - M[0][2]*M[2][1]),(M[0][0]*M[2][2] - M[0][2]*M[2][0]),
     -(M[0][0]*M[2][1] - M[0][1]*M[2][0])},{(M[0][1]*M[1][2] - M[0][2]*M[1][1]),-(M[0][0]*M[1][2] - M[0][2]*M[1][0]),(M[0][0]*M[1][1] - M[0][1]*M[1][0])}};
 
@@ -19,22 +35,27 @@ int main(){
 
   int identity[3][3] = {};
 
-  // std::cout << "before" << cofactor[0][2] << std::endl;
+  std::cout<< "\nThe inverse matrix of M is: " << std::endl;
 
   for (int i = 0; i < 3; i++){ //iterate through every row
 
+    std::cout << " " << std::endl;
+
     for (int j = 0; j < 3; j++){ //iterate through every column
-
+      float inverse = 0;
       t_cofactor[i][j] = cofactor[j][i]; //transpose successful
-
-      std::cout << "cofactor transposed"<< t_cofactor[i][j] << std::endl;
+      inverse = t_cofactor[i][j]*1./determinant;
+      std::cout << inverse << " " << std::flush;
 
     }
+
   }
 
-  // std::cout << "after" << t_cofactor[2][0] << std::endl;
+  std::cout << "\n \nThe matrix multiplication between M and the inverse of M:" << std::endl;
 
   for (int i = 0; i < 3; i++){ // i is row iteration
+
+    std::cout << " " << std::endl;
 
     for (int j = 0; j < 3; j++){ // j is column iteration
 
@@ -44,40 +65,36 @@ int main(){
         I += M[i][k]*t_cofactor[k][j];//*1./determinant; //using determinant here will give results close to identity matrix, except first time is equals to -2, why?!
         // identity[i][j] += M[i][k]*t_cofactor[k][j]/determinant; // i suspect using += on arrays is not good practice
 
-
-        // std::cout << "i:" << i << " j:" << j << " k:" << k << "\t" << result << std::endl;
-
       }
       identity[i][j] = I/determinant; // this is where the matrix multiplication between M and M_inverse completes, an identity matrix is found
-      std::cout << identity[i][j] << std::endl;
+
+      std::cout << identity[i][j] << " " << std::flush;
 
     }
   }
 
-  // for(int i = 0; i <3; i++){
-  //   for (int j =0; j < 3; j++){
-  //
-  //       // std::cout << identity[i][j] << std::endl;
-  //
-  //     if (j==2){
-  //       std::cout << "\n" << std::endl;
-  //
-  //     }
-  //   }
-  // }
+  std::cout << "\n \nis an identity matrix." << std::endl;
 
   // QUESTION 4 - Bubble Sort
+
+  std::cout << "\nQuestion 4: Bubble Sort\n" << std::endl; //clears some space for Q4
+
 
   int sequence[24] = {1,5,17,3,75,4,4,23,5,12,34,34,805,345,435,234,6,47,4,9,0,56,32,78};
   int sequence_size = sizeof(sequence)/sizeof(int);
   bool sorted = false;
   int sort = 0;
 
+  std::cout << "Before:" << std::endl;
+
   for(int j = 0; j < sequence_size; j++){
 
-    std::cout << "before j = " << j << "\t" << sequence[j] << std::endl;
+    std::cout << sequence[j] << " " << std::flush;
 
   }
+
+  std::cout << " " << std::endl;
+
   start:
   for (int i = 0; i < sequence_size; i++){
 
@@ -96,13 +113,15 @@ int main(){
 
   if(sorted == true){
 
-    std::cout << "The sequence has been sorted. Here are the results:\n" << std::endl;
+    std::cout << "\nThe sequence has been sorted. Here are the results:" << std::endl;
 
     for(int j = 0; j < sequence_size; j++){
 
-      std::cout << "after j = " << j << "\t" << sequence[j] << std::endl;
+      std::cout << sequence[j] << " " << std::flush;
+      // std::cout << sequence[j] << std::endl;
 
     }
+    std::cout << "\n" << std::endl;
 
 
 }
