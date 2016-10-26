6c6c0dbc81d6e54e7c5957f3afd4fc783a1824de
AndrewYongZhenNing <ayzn95@gmail.com>
2016-10-26 08:53:45 +0100

    Add files via upload

--- AYONG_PS4Q4.cpp
+++ AYONG_PS4Q4.cpp
@@ -5,52 +5,32 @@ int main(){
 
   std::cout << "Question 4a" << std::endl;
 
-  /*Example
-  // int p = 5;
-  // int q = 6;
-  // int *r;
-  // r = &q;
-
-  // std::cout << *r << std::endl; // giving the * dereferences it; without the star gives the memory address
-  */
 
 
   int sequence_1[]= {1,3,12,-1,47,4,4,23,35,12,34,34,809};
-  int sequence_1_length = sizeof(sequence_1)/sizeof(int);
+  int sequence_1_length = sizeof(sequence_1)/sizeof(int); // computes the size of the sequence here, to be used in functions later
 
   int sequence_2[] = {945,432,134,79,47,4,90,0,-56,32,78};
   int sequence_2_length = sizeof(sequence_2)/sizeof(int);
 
-  
+  int sequence[24] = {0}; // initialise an array the size of the two arrays combined
 
-  int sequence[24] = {};
-
-  int test[] = {1,3,12,-1,47,4,4,23,35,12,34,34,809,945,432,134,79,47,4,90,0,-56,32,78};
-  int test_len = sizeof(test)/sizeof(int);
-
-  // std::cout << sequence_1[31] << std::endl; // beyond the allocated index, the array outputs random values
 
   sort(sequence_1, sequence_1_length);
   sort(sequence_2, sequence_2_length);
 
+
   std::cout << "Question 4b" << std::endl;
 
   merge(sequence, sequence_1, sequence_1_length, sequence_2, sequence_2_length);
-  // sort(test,test_len);
 
   std::cout << "Question 4c" << std::endl;
 
-  std::cout << "Please find the solution to Question 4c in the PDF file." << std::endl;
-
-  /*
-  best case: if two sorted lists join to become a complete sorted list of ascending order: iteration = 1 (the one iteration that checks it is all in ascending order)
-  worst case: if the first value of the second sorted list is smaller than the first value of the first sorted list: iteration = n, where n is the length of the first sorted list
-  */
-
+  std::cout << "Please find the solution to Question 4c in the PDF file.\n" << std::endl;
 
 }
 
-void swap(int *left, int *right){
+void swap(int *left, int *right){ // standard swapping function using pointers as to save memory from declaring new variables for swap
 
   int temp = *left;
   *left = *right;
@@ -106,29 +86,65 @@ void sort(int sequence[], int length){
 
       }
 
-      std::cout << "\nTotal number of iterations: " << iteration_number << "\n" << std::endl;
+      // std::cout << "\nTotal number of iterations: " << iteration_number << "\n" << std::endl; // MARKER: for your interest, I just wanted to know out of curiosity how many iterations it takes
 
     }
 
 
     }
+
+  std::cout << "\n" << std::endl;
 
 
 }
 
 void merge(int main_array[], int sequence1[], int sequence1_length, int sequence2[], int sequence2_length){
 
-  for (int i = 0; i < sequence1_length+sequence2_length; i++){
+	int i = 0; // counter for sequence 1
+	int j = 0; // counter for sequence 2
+	int k = 0; // counter for main_array
 
-    if(i < sequence1_length){
-      main_array[i] = sequence1[i]; // append first sequence into main array
-    }
+	while (i < sequence1_length && j < sequence2_length){ // the AND statement is designed to ensure no overflow if one of the sequence is smaller than the other
+		if (sequence1[i] < sequence2[j]){
+			main_array[k] = sequence1[i];
+			i++;
+			k++;
 
-    else if(i >= sequence1_length){
-      main_array[i] = sequence2[i-sequence1_length]; // appends second sequence after first sequence is in array
-    }
-  }
+		}
 
-  sort(main_array,sequence1_length+sequence2_length);
+		else if (sequence1[i] >= sequence2[j]){
+			main_array[k] = sequence2[j];
+			j++;
+			k++;
+
+		}
+
+	}
+
+	if (i == sequence1_length){ // when sequence 1 is used up; i == sequence1_length rather than i < sequence_length is due to the condition in the if lock, namely tha after each operation i is incremented, thus after the last operation the while loop allows for i, i has been incremented once more before the loop is exited. Hence the current value of i == sequnce1_length (the same would apply in the ase of j if sequence2 causes the end of the while loop)
+		for (j; j < sequence2_length; j++){
+			main_array[k] = sequence2[j];
+			k++;
+
+		}
+	}
+
+
+
+	else if (j == sequence2_length ){ // when sequence 2 is used up
+		for (i; i < sequence1_length; i++){
+			main_array[k] = sequence1[i];
+			k++;
+		}
+	}
+
+	std::cout << "The sequence has been sorted. Here is the result: " << std::endl;
+
+	for (int a = 0; a < sequence1_length + sequence2_length; a ++){
+
+		std::cout << main_array[a] << " " << std::flush;
+	}
+
+	std::cout << " \n" << std::endl; //start new line after flush
 
 }
